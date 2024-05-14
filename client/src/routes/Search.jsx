76ed0b2df141aa38549a5a5b9dbea3086995095d
Arijit Debnath <itsarijitdebnath@gmail.com>
2024-05-14 10:08:10 +0530

    Frontend client and Backend with DB integration

---
+++ client/src/routes/Search.jsx
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom";
+import Search from "../components/Search";
+
+const SearchView = () => {
+  const param = useParams();
+  console.log(param.query);
+  return <Search />;
+};
+
+export default SearchView;
